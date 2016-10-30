a434796822dd2bc77b638b9b04f10090ff707d8b
Vadim <vadimgordenya@gmail.com>
2016-10-30 16:35:00 +0300

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,18 @@
+   $(document).ready(function () {
+        $(".sub > a").click(function() {
+            var ul = $(this).next(),
+                    clone = ul.clone().css({"height":"auto"}).appendTo(".mini-menu"),
+                    height = ul.css("height") === "0px" ? ul[0].scrollHeight + "px" : "0px";
+            clone.remove();
+            ul.animate({"height":height});
+            return false;
+        });
+           $('.mini-menu > ul > li > a').click(function(){
+           $('.sub a').removeClass('active');
+           $(this).addClass('active');
+        }),
+           $('.sub ul li a').click(function(){
+           $('.sub ul li a').removeClass('active');
+           $(this).addClass('active');
+        });
+    });
